37d67fe24ff37681c70405765c337b44ef648000
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-09-27 13:03:12 +0300

    pre deploy

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,20 +1,8 @@
-/* const PrivateRoute = () => {
-  return (
-    <div>
-      PrivateRoute
-    </div>
-  )
-}
-
-export default PrivateRoute */
-
 import { Route, Navigate } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, user } = useAuth0()
   const isUser = isAuthenticated && user
-  console.log(isAuthenticated)
-  console.log(user)
   if (!isUser) {
     return <Navigate to='/login' />
   }
@@ -22,20 +10,3 @@ const PrivateRoute = ({ children }) => {
 }
 
 export default PrivateRoute
-
-/* import { Route, Navigate } from 'react-router-dom'
-import { useAuth0 } from '@auth0/auth0-react'
-// Dashboard will be a child of private route - other collecting values
-const PrivateRoute = ({children, ...rest}) => {
-  const isUser = true
-  return (
-    <Route
-      {...rest}
-      render={()=>{
-        return isUser ? children : <Navigate to='/login' />
-      }}
-    ></Route>
-  )
-}
-
-export default PrivateRoute */
