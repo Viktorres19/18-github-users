bcd116933f1fa6140bc12c512d3144c00d242f1f
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-09-27 11:01:04 +0300

    first commit 660

--- src/components/Charts/Doughnut2d.jsx
+++ src/components/Charts/Doughnut2d.jsx
@@ -1,9 +1,27 @@
-const Doughnut2d = () => {
-  return (
-    <div>
-      Doughnut2d
-    </div>
-  )
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const Doughnut2d = ({data}) => {
+  const chartConfigs = {
+    type: 'doughnut2d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      chart: {
+        caption: 'Stars Per Language',
+        decimals: 0,
+        doughnutRadius: '45%',
+        showPercentValues: 0,
+        theme: 'candy',
+      },
+      data
+    }
+  };
+  return (<ReactFC {...chartConfigs} />)
 }
 
 export default Doughnut2d
